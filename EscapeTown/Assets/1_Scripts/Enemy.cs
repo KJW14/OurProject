cede074e477457b3c43a26326e537957ec109b0b
HeoJongHyun <ghkfls126@naver.com>
2020-09-28 21:05:04 +0900

    Script Update

---
+++ EscapeTown/Assets/1_Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Human
+{
+    // HP
+    // 공격력
+    // 이동속도
+
+    // 대기()
+    // 걷기()
+    // 공격()
+    // 죽음()
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnDrawGizmos();
+    }
+    void EnemyRaycast()
+    {
+    }
+    private void OnDrawGizmos()
+    {
+        float maxDistance = 40;
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, maxDistance, transform.forward, 0);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxDistance);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log("collider : " + hit.collider.name);
+                Player player = hit.collider.GetComponent<Player>();
+
+                if (player != null)
+                {
+                    Debug.Log("Player 찾음!");
+                    Vector3 direction = (player.transform.position - transform.position).normalized;
+                    Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+                    transform.position += direction * moveSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+}
---
+++ EscapeTown/Assets/1_Scripts/Human.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Human : MonoBehaviour
+{
+    // HP
+    // 공격력
+    // 이동속도
+    // 걷기()
+    // 뛰기()
+    // 공격()
+    // 죽음()
+
+    // 보류
+    // 대기()
+    public GameObject obj;
+    public Animator animator;
+    // HP
+    protected int hp;
+    // 공격력
+    protected int ap;
+    // 이동속도
+    protected float moveSpeed;
+
+    // 공격()
+    protected void Attack()
+    {
+
+    }
+    // 죽음()
+    public void Die()
+    {
+
+    }
+    // 대기()
+    void Idle()
+    {
+
+    }
+}
